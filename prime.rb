92f548fcfb587aeedbd03b2052064d24a603be8f
Wills Blake <richwblake@gmail.com>
2019-11-06 02:09:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?( number )
+  return false if number <= 1
+  
+  count = 2
+  while count < number do
+    return false if number % count == 0
+    count += 1
+  end
+  true
+end
